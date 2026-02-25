1c8b51675bd111e695d81f9b9aa1b90f686c6929
OlcsikaSZ <146717305+OlcsikaSZ@users.noreply.github.com>
2026-02-25 14:53:52 +0100

    Enable additional lighting sources in app.c

--- app/src/app.c
+++ app/src/app.c
@@ -98,6 +98,8 @@ void init_opengl()
 
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
+    glEnable(GL_LIGHT2);
 
     glEnable(GL_COLOR_MATERIAL);
     glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
